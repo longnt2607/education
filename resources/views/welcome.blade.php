622df6127845fcbc2bedfde259d593b61f5f0d28
longnt2607 <ntlong12c1ncthanh@gmail.com>
2021-03-17 08:29:46 +0700

    dung cau truc web education

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="/assets/client/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-primary">hello</h1>
+    <a href="" class="btn btn-primary">hi</a>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
